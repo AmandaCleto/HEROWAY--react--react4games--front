653827e6c3dbacfa2d55abc378052c29e50a00c2
AmandaCleto <amanda.clet@gmail.com>
2021-09-04 16:21:08 -0300

    build folder for github pages

---
+++ build/precache-manifest.b67d5340f1cc6e786fb12ff5c70d06f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e86b3287152b399e6a2de746900fd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab9b5b58a8a7a2d54b02",
+    "url": "/static/css/main.672ed726.chunk.css"
+  },
+  {
+    "revision": "4a3e0c84c7ba2cb79950",
+    "url": "/static/js/2.b29c5e45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b29c5e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9b5b58a8a7a2d54b02",
+    "url": "/static/js/main.04c5bdf9.chunk.js"
+  },
+  {
+    "revision": "5d549ad2888317a819a6",
+    "url": "/static/js/runtime-main.7348daea.js"
+  }
+]);
